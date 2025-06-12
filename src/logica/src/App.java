00f27b632b2786ec36f7507e777eb519c915835c
Gustavo Zequim Maldonado <gustavozequim9@gmail.com>
2025-06-12 13:47:21 +0000

    primeiro exercicio feito

--- src/logica/src/App.java
+++ src/logica/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int n = 5;
+        if (n%2==0) {
+            System.out.println("Par!");
+        }
+        else{
+            System.out.println("Impar!");
+        }
     }
 }
