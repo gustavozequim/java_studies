4a2b6d270a807de9798992f3ddb8906cd5736f30
Gustavo Zequim Maldonado <gustavozequim9@gmail.com>
2025-06-12 14:03:19 +0000

    Print formatado e estudo de variavesis: String, int e double

---
+++ src/logica/src/Variaveis.java
@@ -0,0 +1,8 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        String nome = "Carlos";
+        int idade = 23;
+        double altura = 1.75;
+        System.out.printf("\nAltura: "+ altura + ", Idade: " + idade + ", Nome: " + nome);
+    }
+}
