f609d0727b4d8a256dbbad1bebe066c60b1b0ac5
Gustavo Zequim Maldonado <gustavozequim9@gmail.com>
2025-06-18 11:57:49 +0000

    Mudanca no arquivo 'Excecoes'

--- src/Excecoes.java
+++ src/Excecoes.java
@@ -1,11 +1,13 @@
 public class Excecoes{
     public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
         try{
-            int resultado = 10/0;   
+            int resultado = a/b;   
         }catch (ArithmeticException e) {
-            System.out.println("Divisão por zero não é permitida: " + e.getMessage());
+            System.out.printf("Divisão de " + a + " e " + b + " não é permitida: " + e.getMessage());
         }finally {
-            System.out.println("Bloco finally executado.");
+            System.out.println("\nBloco finally executado.");
         }
     }
 }
