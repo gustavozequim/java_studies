b88bd9caf0fddf4b467687b63ee7573d7b118d88
Gustavo Zequim Maldonado <gustavozequim9@gmail.com>
2025-08-12 19:07:32 +0000

    em andamento CRUD

--- src/Crud_em_memoria.java
+++ src/Crud_em_memoria.java
@@ -1,4 +1,6 @@
+
 import java.util.HashMap;
+
 import java.util.Map;
 import java.util.Scanner;
 
@@ -9,54 +11,73 @@ public class Crud_em_memoria {
         livros.put("Sociedade do Anel", "JRR Tolkien");
         livros.put("As Duas Torres", "JRR Tolkien");
         livros.put("O Retorno do Rei", "JRR Tolkien");
-        System.out.println("Segue abaixo as opcoes, o que deseja fazer?:");
-        System.out.println("""
-        [1] Listar livros
-        [2] Adicionar Livro
-        [3] Editar Livro
-        [4] Apagar Livro
-        [5] Sair
-        """);
-        int opcao = cmndLine.nextInt();
-        
-        switch (opcao) {
-        case 1:
-            livros.forEach((titulo, autor) -> System.out.println("Autor: " + autor + ", Titulo: " + titulo + "\n"));
-            break;
-        case 2:
-            System.out.print("\nOK! Vamos la.\n");
-            cmndLine.nextLine();
-            System.out.println("Titulo: ");
-            String novoLivroTit = cmndLine.nextLine();
-            System.out.println("Autor: ");
-            String novoLivroAut = cmndLine.nextLine();
-            livros.put(novoLivroTit, novoLivroAut);
-            System.out.println("\nNovo livro adicionado: " + novoLivroTit + ".\n");
-            livros.forEach((titulo, autor) -> System.out.println("Autor: " + autor + ", Titulo: " + titulo));
-            break;
+        boolean sairDoWhile = false;
+        while (true) {
+            System.out.println("Segue abaixo as opcoes, o que deseja fazer?:");
+            System.out.println("[1]Listar livros");
+            System.out.println("[2] Adicionar Livro");
+            System.out.println("[3] Editar Livro");
+            System.out.println("[4] Apagar Livro");
+            System.out.println("[5] Sair");
+            int opcao = cmndLine.nextInt();
+            
+            switch (opcao) {            
+            case 1:
+                livros.forEach((titulo, autor) -> System.out.println("Autor: " + autor + ", Titulo: " + titulo + "\n"));
+                break;
+            case 2:
+                System.out.print("\nOK! Vamos la.\n");
+                cmndLine.nextLine();
+                System.out.println("Titulo: ");
+                String novoLivroTit = cmndLine.nextLine();
+                System.out.println("Autor: ");
+                String novoLivroAut = cmndLine.nextLine();
+                livros.put(novoLivroTit, novoLivroAut);
+                System.out.println("\nNovo livro adicionado: " + novoLivroTit + ".\n");
+                livros.forEach((titulo, autor) -> System.out.println("Autor: " + autor + ", Titulo: " + titulo));
+                continue;
 
-        case 3:
-            System.out.print("\nDigite o livro que deseja editar\n");
-            cmndLine.nextLine();
-            System.out.println("O que deseja editar?:");
-            String editLivro = cmndLine.nextLine();
-            if (editLivro.toLowerCase() == "titulo"){
-                String novoTitulo = cmndLine.nextLine();
-                livros.put(editLivro, novoTitulo);
+            case 3:
+                System.out.print("\nDigite o livro que deseja editar\n");
+                cmndLine.nextLine();
+                String editLivro = cmndLine.nextLine();
+                System.out.println("O que deseja editar? Digite T para titulo e A para autor:");
+                cmndLine.nextLine();
+                String opEdit = cmndLine.nextLine();
+                if (opEdit.toLowerCase() == "a"){
+                    String novoAutor = cmndLine.nextLine();
+                    livros.put(editLivro, novoAutor);
+                }
+                if (opEdit.toLowerCase() == "t"){
+                    String aut = livros.get(editLivro);
+                    livros.remove(editLivro);
+                    System.out.println(aut);
+                    System.out.println("Digite o novo titulo:");
+                    String novoTitulo = cmndLine.nextLine();
+                    livros.put(novoTitulo, aut);
+                    System.out.println("Lista atualizada:");
+                    livros.forEach((titulo, autor) -> System.out.println("Autor: " + autor + ", Titulo: " + titulo));
+                }
+                continue;
+
+            case 4:
+                System.out.print("\nDigite o livro que deseja apagar (Titulo igual ao cadastrado)\n");
+                cmndLine.nextLine();
+                String rmvLivro = cmndLine.nextLine();
+                System.out.print("\nApagando...\n");
+                livros.remove(rmvLivro);
+                System.out.println("Lista de livros atualizadas:");
+                livros.forEach((titulo, autor) -> System.out.println("Autor: " + autor + ", Titulo: " + titulo));
+                continue;
+            case 5:
+                sairDoWhile = true;
+                break;
             }
-            else{
-
+            if (sairDoWhile) {
+                break;
             }
-            break;
-
-        default:
-            System.out.print("\nOpção Inválida!");
-            break;
-
-        case 4:
-            System.out.print("\nAté logo!");
-            cmndLine.close();       
         }
-
+        cmndLine.close();
+        System.out.println("\nAte logo!\n");
     }
 }
